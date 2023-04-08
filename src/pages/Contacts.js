f75a2c690c68ad71fe6c8d7458d07661600a4e42
Annaivv <annashalya@gmail.com>
2023-04-08 16:49:44 +0300

    fetchContacts added

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,10 +1,17 @@
 import { ContactEditor } from 'components/ContactEditor/ContactEditor';
 import { ContactList } from 'components/ContactList/ContactList';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/contacts/operations';
 import { selectLoading } from 'redux/contacts/selectors';
 
 export default function Contacts() {
   const isLoading = useSelector(selectLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <>
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -3,6 +3,18 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
 export const addContact = createAsyncThunk(
   'tasks/addContact',
   async ({ name, number }, thunkAPI) => {
